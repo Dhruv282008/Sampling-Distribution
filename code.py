5e5abe47a3ff31a128d777a7a9e50017a3327665
Dhruv282008 <66867893+Dhruv282008@users.noreply.github.com>
2021-09-01 15:00:22 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,58 @@
+import plotly.figure_factory as ff 
+import statistics
+import pandas as pd 
+import random    
+import plotly.graph_objects as go
+df = pd.read_csv("data.csv")
+data = df["average"].to_list()
+
+fig1 = ff.create_distplot([data], ["Average"])
+fig1.show()
+
+def randomSetOfMean(counter):
+    dataset = []
+    for i in range(0, counter):
+        random_index = random.randint(0, len(data)-1)
+        value = data[random_index]
+        dataset.append(value)
+
+    mean = statistics.mean(dataset)
+
+    return mean
+
+def graph(meanlist):
+    mean = statistics.mean(meanlist)
+    fig2 = ff.create_distplot([meanlist], ["MEAN OF MEANLIST"])
+    fig2.add_trace(go.Scatter(x = [mean, mean], y = [0, 10], mode = "lines", name = "Mean"))
+    fig2.show()
+
+def setup():
+    meanlist = []
+    for i in range(0, 1000):
+        setOfMeans = randomSetOfMean(100)
+        meanlist.append(setOfMeans)
+
+    graph(meanlist)
+
+    mean = statistics.mean(meanlist)
+
+    print("Mean of Sample Distribution: ", mean)
+
+def stdev():
+    meanlist = []
+    for i in range(0, 1000):
+        setOfMeans = randomSetOfMean(100)
+        meanlist.append(setOfMeans)
+
+        stdev = statistics.stdev(meanlist)
+
+    print("Standard Deviation of Sample Distribution: ", stdev)
+
+setup()
+stdev()
+
+mean = statistics.mean(data)
+print("Mean of Data(Population): ", mean)
+
+stdev = statistics.stdev(data)
+print("Std Deviation of Data(Population): ", stdev)
